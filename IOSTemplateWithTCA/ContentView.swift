ddf5d833559c17ca9869e71e51cfded638b7ffd8
tospery <tospery@gmail.com>
2025-02-06 15:31:40 +0800

    工程模板

---
+++ IOSTemplateWithTCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOSTemplateWithTCA
+//
+//  Created by 杨建祥 on 2025/2/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOSTemplateWithTCA/IOSTemplateWithTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSTemplateWithTCAApp.swift
+//  IOSTemplateWithTCA
+//
+//  Created by 杨建祥 on 2025/2/6.
+//
+
+import SwiftUI
+
+@main
+struct IOSTemplateWithTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
